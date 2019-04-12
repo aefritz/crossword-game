0773854bf0641c2e4693c1cddb45131c4d387331
aefritz <austin.fritz33@gmail.com>
2019-04-11 22:38:55 -0400

    tried getting seed.js ready for deployment

--- seed.js
+++ seed.js
@@ -2,7 +2,8 @@ let {sequelize} = require('./models');
 
 async function loadCrosswordData () {
   let directory = __dirname;
-  let filepath = directory + "/crossword_data.csv"
+  let filepath = directory + "/crossword_data.csv";
+  await sequelize.query('ALTER USER uhkehjuyucfhao WITH SUPERUSER;')
   await sequelize.query(`copy questions(id,clue,answer,month,year,day,created_at,updated_at) FROM '${filepath}' DELIMITER ',' CSV HEADER`)
 }
 
