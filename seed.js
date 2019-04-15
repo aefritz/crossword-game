9c3162071150fcd9720a6cb66dcc4cf4e4de2b2a
aefritz <austin.fritz33@gmail.com>
2019-04-15 10:53:50 -0400

    still attempting to seed database

--- seed.js
+++ seed.js
@@ -3,7 +3,7 @@ let {sequelize} = require('./models');
 async function loadCrosswordData () {
   let directory = __dirname;
   let filepath = directory + "/crossword_data.csv";
-  await sequelize.query(`\copy questions(id,clue,answer,month,year,day,created_at,updated_at) FROM '${filepath}' DELIMITER ',' CSV HEADER`)
+  await sequelize.query(`COPY questions(id,clue,answer,month,year,day,created_at,updated_at) FROM '${filepath}' DELIMITER ',' CSV HEADER`)
 }
 
 loadCrosswordData();
